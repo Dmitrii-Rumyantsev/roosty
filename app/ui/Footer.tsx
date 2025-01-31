aac5fea76e7925121b8715076e14c40261ef81e0
Dmitrii Rumyantsev <dima33773@gmail.com>
2025-01-31 12:28:26 +0300

    Start project

---
+++ app/ui/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+      <footer className="bg-white p-4 mt-auto text-center">
+        <div className="container">
+          <a href="#" className="text-muted mx-2">Пользовательское соглашение</a>
+          <a href="#" className="text-muted mx-2">Условия оплаты</a>
+        </div>
+      </footer>
+    );
+  }
+  
